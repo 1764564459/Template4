13195b65d965d23173fb56671c4404bbdc2966b3
li join <leeds92@163.com>
2018-12-05 17:49:42 +0800

    添加项目文件。

---
+++ NFine/NFine.Domain.Entity/ControlTemp.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+namespace NFine.Domain.Entity.SystemManage
+{ 
+
+//创建保存控制器文件夹
+
+                                                                                                                                                                                                                                                                                                                                                    }
---
+++ NFine/NFine.Domain.Entity/NFineEntity.cs
@@ -0,0 +1,656 @@
+﻿
+
+
+
+
+//------------------------------------------------------------------------------
+// <T4模板生成实体类/>
+//     此代码由T4模板自动生成
+//	   生成时间 2018-12-05 17:48:03
+//     对此文件的更改可能会导致不正确的行为，并且如果重新生成代码，这些更改将会丢失。
+// <T4模板生成实体类/>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+namespace NFine.Domain.Entity.SystemManage
+{ 
+           
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+              
+  //OutPut
+                                                                                                                                                                                                                                                                                                                                                          
+  //分页
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.MigrationHistory.Models.MigrationHistory.Input
+     {
+         public class MigrationHistoryPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Account.Models.Account.Input
+     {
+         public class AccountPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Department.Models.Department.Input
+     {
+         public class DepartmentPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Menu.Models.Menu.Input
+     {
+         public class MenuPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Role.Models.Role.Input
+     {
+         public class RolePagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.RoleMenus.Models.RoleMenus.Input
+     {
+         public class RoleMenusPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.User.Models.User.Input
+     {
+         public class UserPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.UserMenus.Models.UserMenus.Input
+     {
+         public class UserMenusPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.UserRoles.Models.UserRoles.Input
+     {
+         public class UserRolesPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.UserSite.Models.UserSite.Input
+     {
+         public class UserSitePagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Camera.Models.Camera.Input
+     {
+         public class CameraPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.CameraToken.Models.CameraToken.Input
+     {
+         public class CameraTokenPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Equip.Models.Equip.Input
+     {
+         public class EquipPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.FogMechine.Models.FogMechine.Input
+     {
+         public class FogMechinePagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Supplier.Models.Supplier.Input
+     {
+         public class SupplierPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.AlarmInfo.Models.AlarmInfo.Input
+     {
+         public class AlarmInfoPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Day.Models.Day.Input
+     {
+         public class DayPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.ExceedTag.Models.ExceedTag.Input
+     {
+         public class ExceedTagPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Hour.Models.Hour.Input
+     {
+         public class HourPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Minute.Models.Minute.Input
+     {
+         public class MinutePagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Real.Models.Real.Input
+     {
+         public class RealPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Site.Models.Site.Input
+     {
+         public class SitePagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.SiteCamera.Models.SiteCamera.Input
+     {
+         public class SiteCameraPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.SiteEquip.Models.SiteEquip.Input
+     {
+         public class SiteEquipPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Config.Models.Config.Input
+     {
+         public class ConfigPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.DicGroup.Models.DicGroup.Input
+     {
+         public class DicGroupPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Dictionary.Models.Dictionary.Input
+     {
+         public class DictionaryPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Factor.Models.Factor.Input
+     {
+         public class FactorPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Files.Models.Files.Input
+     {
+         public class FilesPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Log.Models.Log.Input
+     {
+         public class LogPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Region.Models.Region.Input
+     {
+         public class RegionPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.SiteFactor.Models.SiteFactor.Input
+     {
+         public class SiteFactorPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+             using GreenUnity.Common;
+     using GreenUnity.EntityFramework.Entities;
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+     using System.Threading.Tasks;
+     
+     namespace GreenUnity.WebApi.Areas.Token.Models.Token.Input
+     {
+         public class TokenPagingInput: PagingBaseInput
+         {
+             /// <summary>
+             /// 关键字
+             /// </summary>
+             public string Keyword { get; set; }
+         }
+     }
+
+        }
+
+
+
